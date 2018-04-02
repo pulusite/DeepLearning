7b231c666b33d0d74d923c109cd8b18420865d71
pulusite <pulusite@126.com>
2018-04-02 22:33:09 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,16 +1,22 @@
 //
-// Created by dong Zhang on 11/1/17.
+// Created by dong Zhang on 12/25/17.
 //
-
+#include <stdio.h>
 #include <iostream>
-#include <vector>
-#include "header/Circle.h"
+#include <algorithm>
+#include <unordered_set>
+#include <queue>
 
 using namespace std;
 
-int main() {
-    cout << "Hello World"<<endl;
-    Circle c(3);
-    cout<<"Area="<<c.Area()<<endl;
-    return 1;
-}
+class lec0901{
+public:
+    int ladderLength(const string& start,const string &end,const unordered_set<string> &dict){
+        queue<string> current,next;
+        unordered_set<string> visited;
+
+        int level=0;
+        bool found=false;
+        
+    }
+};
